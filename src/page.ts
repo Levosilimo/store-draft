a5d1a4800e4dfc0c552dac9fee2654f0047f395e
Levosilimo <silin.lev@gmail.com>
2022-12-20 12:26:47 +0100

    fix: fix files

--- src/page.ts
+++
@@ -1,5 +0,0 @@
-export default abstract class Page {
-    public abstract HtmlElementInstance: HTMLElement;
-    protected abstract createHtmlElement(): HTMLElement;
-    public abstract hideHtmlElement(): void;
-}
